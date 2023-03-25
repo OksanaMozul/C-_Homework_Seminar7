f035a7a2e9b4decbf16b685d4dd18a08fce4a74d
Oksana <oksana-k83@mail.ru>
2023-03-25 14:32:49 +0300

    Задача_2_Семинар7

--- Task1-3.cs
+++ Task1-3.cs
@@ -47,7 +47,33 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
-
+// Console.Write("Введите строку: ");
+// int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+// Console.Write("Введите столбец: ");
+// int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+// int n = 8; // не больше 8 строк
+// int m = 9; // не больше 9 столбцов
+// Random random = new Random();
+// int[,] arr = new int[n, m];
+// Console.WriteLine("Массив: ");
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i, j] = random.Next(10, 99);
+//         Console.Write("{0} ", arr[i, j]);
+//     }
+//     Console.WriteLine();
+// }
+// if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+// {
+//     Console.WriteLine("Такого элемента нет ");
+// }
+// else
+// {
+//     Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+// }
+// Console.ReadLine();
 
 
 
