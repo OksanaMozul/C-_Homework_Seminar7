0f77e4e84d04fd1f9c292cfc76ba664a6a15d1b4
Oksana <oksana-k83@mail.ru>
2023-03-25 14:44:53 +0300

    Задача_3_Семинар7

--- Task1-3.cs
+++ Task1-3.cs
@@ -6,38 +6,38 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-// Console.WriteLine("Введите количество строк:  ");
-// int linesVol = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите количество столбцов: ");
-// int columnsVol = Convert.ToInt32(Console.ReadLine());
-// double[,] numbers = new double[linesVol, columnsVol];
-// FillArrayRandomNumbers(numbers);
-// PrintArray(numbers);
+Console.WriteLine("Введите количество строк:  ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
 
-// void FillArrayRandomNumbers(double[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
-//         }
-//     }
-// }
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
 
-// void PrintArray(double[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         Console.Write("[ ");
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write(array[i, j] + " ");
-//         }
-//         Console.Write("]");
-//         Console.WriteLine("");
-//     }
-// }
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
 
 
 // Задача 2. Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
@@ -47,33 +47,33 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
-// Console.Write("Введите строку: ");
-// int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
-// Console.Write("Введите столбец: ");
-// int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-// int n = 8; // не больше 8 строк
-// int m = 9; // не больше 9 столбцов
-// Random random = new Random();
-// int[,] arr = new int[n, m];
-// Console.WriteLine("Массив: ");
-// for (int i = 0; i < arr.GetLength(0); i++)
-// {
-//     for (int j = 0; j < arr.GetLength(1); j++)
-//     {
-//         arr[i, j] = random.Next(10, 99);
-//         Console.Write("{0} ", arr[i, j]);
-//     }
-//     Console.WriteLine();
-// }
-// if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
-// {
-//     Console.WriteLine("Такого элемента нет ");
-// }
-// else
-// {
-//     Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
-// }
-// Console.ReadLine();
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 8; // не больше 8 строк
+int m = 9; // не больше 9 столбцов
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(10, 99);
+        Console.Write("{0} ", arr[i, j]);
+    }
+    Console.WriteLine();
+}
+if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+    Console.WriteLine("Такого элемента нет ");
+}
+else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
+Console.ReadLine();
 
 
 
@@ -83,4 +83,26 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
+Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+//Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
